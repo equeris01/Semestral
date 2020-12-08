02589e209891ddabacf75d2d78b366b06685ea39
equeris01 <72086855+equeris01@users.noreply.github.com>
2020-12-07 22:51:13 -0500

    Add files via upload

---
+++ Confirmación.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Página de Confirmación</title>
+</head>
+<body>
+	<H1>Su formulario a sido enviado exitosamente </H1>
+		<php>  print("Comprador: $nombre");<br>
+		print("Correo electrónico: $mail")
+		print("Nombre del producto: $producto")
+		print("Cantidad: $cantidad")
+		</php>
+		
+
+	<input type=button name=boton value=Cerrar onclick="window.close()">
+</body>
+</html>
